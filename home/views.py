b797d90a882de644e6b04052762f9a7dc8b7bdf2
Abdullahinur <aabdullahinur@hotmail.com>
2018-05-15 22:33:33 +0300

    Define current view functions home dashboard user login

--- home/views.py
+++ home/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+# View function to display the homepage
+def home(request):
+    home = "This is My Insta-Clone"
+    return render(request, 'home/home.html', {"home": home})
+
+
+# View function to display the user login page
+def user_login(request):
+
+    return render(request, 'Userlogin/login.html')
+
+
+# View function to display the dashboard after login
+def dashboard(request):
+
+    return render(request, 'dashboard/dashboard.html')
