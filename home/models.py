8eb451168a016b9bd66788248e3ee1fe26708bcf
Abdullahinur <aabdullahinur@hotmail.com>
2018-05-16 01:00:34 +0300

    test for profile functions add function to save, test PASS

--- home/models.py
+++ home/models.py
@@ -18,11 +18,16 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Profile(models.Model):
     # user profiles properties
-    user = models.CharField(max_length=30, default='User')
+    username = models.CharField(max_length=30, default='User')
     profile_photo = models.ImageField(upload_to="profile_photos/", null=True)
     bio = models.TextField(default="Awww, Shucks..it's a secret", blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
+    def __str__():
+        return self.username
+
+    def save_profile(self):
+        self.save()
 
 
 
--- home/tests.py
+++ home/tests.py
@@ -9,7 +9,11 @@ class ProfileTestClass(TestCase):
     '''
 
     def setUp(self):
-        '''
-        method that runs at the begginning of each test
-        '''
-        self.profile = Profile(profile_photo='test_profile_photo', bio='test_bio')
+        # Setup fot test instances
+        self.profile = Profile(profile_photo='me', bio='Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')
+
+    def test_save_profile(self):
+        # test the save method for saving profiles
+        self.profile.save_profile()
+        all_profiles = Profile.objects.all()
+        self.assertTrue(len(all_profiles) > 0)
