18f23bd1257643fd45ed51e3e68588a7988b767b
Abdullahinur <aabdullahinur@hotmail.com>
2018-05-16 00:26:58 +0300

    install pillow update Profile model

--- home/models.py
+++ home/models.py
@@ -14,8 +14,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 # For Signals
-from django.db.models.signals import post_save
-from django.dispatch import receiver
+# from django.db.models.signals import post_save
+# from django.dispatch import receiver
 
 
 # Create your models here.
@@ -25,14 +25,26 @@ class Profile(models.Model):
     bio = models.TextField(max_length=500, blank=True)
 
 
-# Define signals so Profile model will be automatically created and updated when user instances are created/updated
-# Hook methods to User Model whenever save event occurs
-@receiver(post_save, send=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
 
 
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
+
+
+
+
+
+
+
+
+
+
+# # Define signals so Profile model will be automatically created and updated when user instances are created/updated
+# # Hook methods to User Model whenever save event occurs
+# @receiver(post_save, send=User)
+# def create_user_profile(sender, instance, created, **kwargs):
+#     if created:
+#         Profile.objects.create(user=instance)
+#
+#
+# @receiver(post_save, sender=User)
+# def save_user_profile(sender, instance, **kwargs):
+#     instance.profile.save()
