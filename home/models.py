39a6f320b905938841d392bbbfbbe5e322306830
Abdullahinur <aabdullahinur@hotmail.com>
2018-05-16 01:09:16 +0300

    fix profile models and finish test for profile

--- home/models.py
+++ home/models.py
@@ -29,6 +29,9 @@ class Profile(models.Model):
     def save_profile(self):
         self.save()
 
+    def delete_profile(self):
+        self.delete()
+
 
 
 
--- home/tests.py
+++ home/tests.py
@@ -17,3 +17,12 @@ class ProfileTestClass(TestCase):
         self.profile.save_profile()
         all_profiles = Profile.objects.all()
         self.assertTrue(len(all_profiles) > 0)
+
+    def test_delete_profile(self):
+        # test whether the function to delete profiles, removes profiles
+        self.profile.save_profile()
+        my_profile = Profile(profile_photo='NicholasCage.png', bio='Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')
+        my_profile.save_profile()
+        self.profile.delete_profile()
+        all_profiles = Profile.objects.all()
+        self.assertTrue(len(all_profiles) == 1)
