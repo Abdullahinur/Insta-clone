fa97e42096ad192b79f037c42c10241fc0b3ad5f
Abdullahinur <aabdullahinur@hotmail.com>
2018-05-16 00:45:38 +0300

    refactor tests and Profile model

--- home/models.py
+++ home/models.py
@@ -13,16 +13,15 @@
 #         ...
 from django.db import models
 from django.contrib.auth.models import User
-# For Signals
-# from django.db.models.signals import post_save
-# from django.dispatch import receiver
 
 
 # Create your models here.
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    profile_photo = models.ImageField(upload_to='profile_photos/')
-    bio = models.TextField(max_length=500, blank=True)
+    # user profiles properties
+    user = models.CharField(max_length=30, default='User')
+    profile_photo = models.ImageField(upload_to="profile_photos/", null=True)
+    bio = models.TextField(default="Awww, Shucks..it's a secret", blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
 
 
@@ -36,6 +35,9 @@ class Profile(models.Model):
 
 
 
+    # For Signals
+    # from django.db.models.signals import post_save
+    # from django.dispatch import receiver
 
 # # Define signals so Profile model will be automatically created and updated when user instances are created/updated
 # # Hook methods to User Model whenever save event occurs
--- home/tests.py
+++ home/tests.py
@@ -4,10 +4,12 @@ from .models import Profile
 
 # Create your tests here.
 class ProfileTestClass(TestCase):
-    # Set up Method
-    def setUp(self):
-        self.abdullahinur = Profile(user='Abdullahinur', profile_photo='me', bio='Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')
+    '''
+    class that test the characteristics of the Profile model
+    '''
 
-    # Testing instance of a USER
-    def test_instance(self):
-        self.assertTrue(isinstance(self.abdullahinur, Profile))
+    def setUp(self):
+        '''
+        method that runs at the begginning of each test
+        '''
+        self.profile = Profile(profile_photo='test_profile_photo', bio='test_bio')
