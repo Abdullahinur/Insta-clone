6a6d6d4dbd0f8dc2f23a50621695efcdc5b8026a
Abdullahinur <aabdullahinur@hotmail.com>
2018-05-16 05:19:07 +0300

    add initial templates to home and registration

--- instaclone/settings.py
+++ instaclone/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'home.apps.HomeConfig',
+    'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
