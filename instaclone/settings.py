4350df8319dff6cb5dbc3c4602f20e516f588291
Abdullahinur <aabdullahinur@hotmail.com>
2018-05-19 12:36:46 +0300

    change setting for db to psycop

--- instaclone/settings.py
+++ instaclone/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'instaclone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'instaclone',
         'USER': 'abdullahinur',
         'PASSWORD': 'totpot90210',
