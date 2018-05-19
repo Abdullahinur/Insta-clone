2c7ab78e39b198132459caf67fc3722ac55a0409
Abdullahinur <aabdullahinur@hotmail.com>
2018-05-19 15:24:24 +0300

    create user registration page

--- home/urls.py
+++ home/urls.py
@@ -2,9 +2,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import url
 from . import views
+from . import views as core_views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+    url(r'^Signup/$', core_views.signup, name='signup'),
     url(r'^UserLogin/$', views.user_login, name='userLogin'),
     url(r'^dashboard/$', views.dashboard, name='dashboard')
 ]
--- home/views.py
+++ home/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 from .models import Image
 
 
@@ -21,3 +23,19 @@ def user_login(request):
 def dashboard(request):
 
     return render(request, 'dashboard/dashboard.html')
+
+
+# View function to display user signup posted_images
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html', {'form': form})
